f6de7b2240b013542eef3b186217ee1dad1e2c69
qtchaos <72168435+qtchaos@users.noreply.github.com>
2024-01-02 04:13:28 +0200

    Final rewordings, `postgres.ssl`, dark/light icon + dir based titles.

--- components/Logo.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <img src="/icon.png" alt="Logo of movie-web" />
-</template>
---
+++ components/global/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <img src="/icon-light.png" alt="Logo of movie-web" class="img-dark" />
+  <img src="/icon-dark.png" alt="Logo of movie-web" class="img-light" />
+</template>
+
+<style scoped>
+img {
+  height: 30px;
+}
+.light .img-dark {
+  display: none;
+}
+.dark .img-light {
+  display: none;
+}
+</style>
